6b042102301bc337a576297a766b49bbde615112
svell <svellucci18@gmail.com>
2021-11-11 20:03:42 -0800

    working pretty well

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ var generateBtn = document.querySelector("#generate");
  function generatePassword(){
   var pwLength = prompt("Please enter desired length of your password (less than 8 and no more than 128)");
   var passwordPool = []
+  var password = []
+  //Check for the length and ensure that its within range
   if (pwLength < 8){
     alert("Please enter a value greater than 8.");
  } else if (pwLength > 128) {
@@ -21,29 +23,43 @@ var generateBtn = document.querySelector("#generate");
     var upperPrompt = confirm("Does your password need uppercase letters?");
     var specialPrompt = confirm("Does your password need special characters?");
     console.log(numbersPrompt, lowerPrompt, upperPrompt, specialPrompt);
+        // If at least one prompt is selected, start adding those chars to the pool of potential chars
         if (!numbersPrompt && !lowerPrompt && !upperPrompt && !specialPrompt){
           alert("Please select at least one character type");
           return
         } 
         if (numbersPrompt) {
-          passwordPool = numbers 
+          for (var i=0; i<numbers.length; i++)  {
+            passwordPool.push(numbers[i])
+          }
         }
-
-
+        if (lowerPrompt) {
+          for (var i=0; i<lowerLetters.length; i++)  {
+            passwordPool.push(lowerLetters[i])
+          }
+        }
+        if (upperPrompt) {
+          for (var i=0; i<upperLetters.length; i++)  {
+            passwordPool.push(upperLetters[i])
+          }
+        }
+        if (specialPrompt) {
+          for (var i=0; i<specialChars.length; i++)  {
+            passwordPool.push(specialChars[i])
+          }
+        } 
+        console.log(passwordPool)
+        //Start building the password by pulling randomly from the pool.
+        
+        for (var i=0; i< pwLength; i++) {
+            var random = Math.floor(Math.random() * passwordPool.length)
+            password.push(passwordPool[random]);
+        }
+        //converts the array into a string without seperators
+        return password.join("");
+        console.log(password)
  }
 }
-
-
-//  function generatePassword() {
-    
-//   var password = "";
-
-//   for (let i = 0; i < pwLength - 1; i++) {   
-//       password += password
-//   }
-
-//   return password;
-//  }
 
 
 // Write password to the #password input
