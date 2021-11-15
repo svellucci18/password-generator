58863b93334e8460d5e47a350597ac5cadcb5fc4
svell <svellucci18@gmail.com>
2021-11-15 12:07:20 -0800

    ensure that if a char is selected it is used

--- script.js
+++ script.js
@@ -22,35 +22,51 @@ var generateBtn = document.querySelector("#generate");
     var lowerPrompt = confirm("Does your password need lowercase letters?");
     var upperPrompt = confirm("Does your password need uppercase letters?");
     var specialPrompt = confirm("Does your password need special characters?");
-    console.log(numbersPrompt, lowerPrompt, upperPrompt, specialPrompt);
+    
         // If at least one prompt is selected, start adding those chars to the pool of potential chars
         if (!numbersPrompt && !lowerPrompt && !upperPrompt && !specialPrompt){
           alert("Please select at least one character type");
           return
         } 
         if (numbersPrompt) {
+          //To ensure it includes a number if a number is selected, start with a random number.
+          var randnum = Math.floor(Math.random() * numbers.length)
+          password.push(numbers[randnum]);
+          console.log(password)
           for (var i=0; i<numbers.length; i++)  {
             passwordPool.push(numbers[i])
           }
         }
         if (lowerPrompt) {
+          //To ensure it includes a lower if a lower is selected, start with a random number.
+          var randlow = Math.floor(Math.random() * lowerLetters.length)
+          password.push(lowerLetters[randlow]);
+          console.log(password)
           for (var i=0; i<lowerLetters.length; i++)  {
             passwordPool.push(lowerLetters[i])
           }
         }
         if (upperPrompt) {
+          //To ensure it includes a upper if a upper is selected, start with a random number.
+          var randupp = Math.floor(Math.random() * upperLetters.length)
+          password.push(upperLetters[randupp]);
+          console.log(password)
           for (var i=0; i<upperLetters.length; i++)  {
             passwordPool.push(upperLetters[i])
           }
         }
         if (specialPrompt) {
+          //To ensure it includes a special if a special is selected, start with a random number.
+          var randspec = Math.floor(Math.random() * specialChars.length)
+          password.push(specialChars[randspec]);
+          console.log(password)
           for (var i=0; i<specialChars.length; i++)  {
             passwordPool.push(specialChars[i])
           }
         } 
         console.log(passwordPool)
+
         //Start building the password by pulling randomly from the pool.
-        
         for (var i=0; i< pwLength; i++) {
             var random = Math.floor(Math.random() * passwordPool.length)
             password.push(passwordPool[random]);
